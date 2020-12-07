3c18be83eb58e2e174251e6ea59c80e270d8f204
Hari Preeth <harikartik2015@gmail.com>
2020-12-07 15:11:19 +0530

    Integrated all methods

--- Task-3/Bar_graph.py
+++ Task-3/Bar_graph.py
@@ -6,10 +6,20 @@ Created on Fri Aug 16 18:33:04 2019
 """
 import matplotlib.pyplot as plt
 
-def bar_graph(x,y,title)
-	plt.bar(x,y)
-	plt.xlabel('Population')
-	plt.ylabel('Cities')
-	plt.title("Population of cities")
-	plt.legend(loc='upper right')
+def bar_chart(x,y,xLabel,yLabel,Title):
+    plt.bar(x,y)
+    plt.xlabel(xLabel)
+    plt.ylabel(yLabel)
+    plt.title(Title)
+    plt.legend(loc='upper right')
 
+def Line_chart(x,y,xLabel,yLabel,Title):
+    plt.xlabel("value")
+    plt.ylabel("square of value")
+    plt.plot(x,y)
+    plt.title(Title)
+
+def pie_chart(x,Label):    
+    plt.axis("equal")
+    plt.title("Selected stream")
+    plt.pie(x,labels = Label, autopct="%1.1f%%")
